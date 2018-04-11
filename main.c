e75d2f7d1687ee7ab7c70300e3155f0b6863f09b
forusers12 <38276570+forusers12@users.noreply.github.com>
2018-04-11 17:01:21 +0900

    Create main.c for a mini calculator

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <stdio.h>
+
+int sum(int, int);
+
+int main()
+{
+	int num1 = 0, num2 = 0;
+
+
+	printf("Input two number : ");
+	scanf_s("%d %d", &num1, &num2);
+	printf("%d \n", sum(num1, num2)); //SUM결과 출력
+
+	return 0;
+}
+
+int sum(int num1, int num2)
+{
+	return num1 + num2;
+}
