bbe595e98570cead377592b96bfc059daf0fcc9e
MinsuJeong <vhfhddljms@gmail.com>
2018-06-07 15:21:54 +0900

    로그인 완료 후 index로 이동

--- htdocs/login/loginserv.php
+++ htdocs/login/loginserv.php
@@ -3,35 +3,36 @@
 
 $error='';
 if(isset($_POST['submit'])){
- if(empty($_POST['user']) || empty($_POST['pass'])){
+ if(empty($_POST['id']) || empty($_POST['password'])){
  $error = "아이디와 패스워드를 입력해주세요";
  }
  else
  {
 
- $user=$_POST['user'];
- $pass=$_POST['pass'];
+ $id=$_POST['id'];
+ $password=$_POST['password'];
  $mysql_hostname = 'localhost';
  $mysql_username = 'root';
  $mysql_password = '111111';
- $mysql_database = 'test';
+ $mysql_database = 'academy';
  $mysql_port = '3307';
  $mysql_charset = 'utf8';
  $conn = new mysqli($mysql_hostname, $mysql_username,
  $mysql_password, $mysql_database, $mysql_port);
 
- if($connect->connect_errno){
-   echo '[연결실패] : '.$connect->connect_error.'';
+ if($conn->connect_errno){
+   echo '[연결실패] : '.$conn->connect_error.'';
+   echo ''.$conn->connect_errno.'';
 } else {
    //echo '[연결성공]';
 }
 
- $query = mysqli_query($conn, "SELECT * FROM userpass WHERE pass='$pass' AND user='$user'");
+ $query = mysqli_query($conn, "SELECT * FROM member WHERE password='$password' AND id='$id'");
 
  $rows = mysqli_num_rows($query);
  if($rows == 1){
 
- header("Location: welcome.php");
+ header("Location: ./index.html");
  }
  else
  {
