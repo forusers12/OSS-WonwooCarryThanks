831f260a5549e1d7c933916f6c6eb562560e7eb8
MinsuJeong <vhfhddljms@gmail.com>
2018-06-08 15:51:56 +0900

    /* 회원가입시 아이디 중복여부 확인과 회원가입 완료시 완료알림 생성*/

--- htdocs/login/signUp.php
+++ htdocs/login/signUp.php
@@ -15,7 +15,7 @@ if($conn->connect_errno){
   echo '[연결실패] : '.$conn->connect_error.'';
   echo ''.$conn->connect_errno.'';
 } else {
-  echo '[연결성공]';
+  //echo '[연결성공]';
 }
 
  $id=$_POST['id'];
@@ -23,10 +23,20 @@ if($conn->connect_errno){
  $password2=$_POST['pwd2'];
  $email=$_POST['email'];
 
+
+ $check = "SELECT * FROM member WHERE id = '{$id}'";
+ $resource = $conn->query($check) or die($this->_connect->error);
+ if($resource->num_rows>=1){
+   echo "<script> alert('이미 존재하는 아이디입니다.');</script>";
+   echo "<script>window.history.back();</script>";
+   exit(0);
+ }
  $sql = "INSERT INTO member (id,password,email,admin) VALUES('$id','$password','$email','0')";
 
  if($conn->query($sql)){
-  echo 'success inserting';
+   echo "<script> alert('회원가입이 완료되었습니다..');</script>";
+   echo "<script>window.history.back();</script>";
+
  }else{
   echo 'fail to insert sql';
  }
