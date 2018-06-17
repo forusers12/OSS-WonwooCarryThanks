4e885e767756b70a5581245cb4ddd8236b39a045
WonWooCho <wonwoo7558@gmail.com>
2018-06-17 17:27:37 +0900

    db다시수정

--- htdocs/db.php
+++ htdocs/db.php
@@ -1,13 +1,13 @@
 <?php
 
 session_start();
-	$db = new mysqli("localhost","root","12309753","db1");
+	$db = new mysqli("localhost","root","111111","academy");
 	$db->set_charset("utf8");
 
 	$host="localhost";
 	$dbid="root";  // "
-	$dbpw="12309753";
-	$dbname="db1";
+	$dbpw="111111";
+	$dbname="oss";
 	function connect_db($host, $dbid, $dbpw, $dbname)
 	{
 	    return mysqli_connect($host, $dbid, $dbpw, $dbname);
