233a23f885bce9b5cc883d7cabee13be974f7c3b
WonWooCho <wonwoo7558@gmail.com>
2018-06-17 17:57:25 +0900

    db수정

--- htdocs/db.php
+++ htdocs/db.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-<<<<<<< HEAD
+
 $db = new mysqli("localhost", "root", "111111", "academy");
 $db->set_charset("utf8");
 
@@ -13,10 +13,9 @@ function connect_db($host, $dbid, $dbpw, $dbname)
 {
   return mysqli_connect($host, $dbid, $dbpw, $dbname);
 }
-=======
-	$db = new mysqli("localhost","root","111111","oss");
+
+	$db = new mysqli("localhost","root","111111","academy");
 	$db->set_charset("utf8");
->>>>>>> 59735164d8a97aed5f434c8621a3071fcc191107
 
 function mq($sql){
   global $db;
