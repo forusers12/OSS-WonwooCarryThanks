be121054ae2fd1e80209fad4c605d2fb847d4c02
Hyunsoo Lee <forusers12@gmail.com>
2018-06-17 19:27:32 +0900

    [개선] 리팩토링

---
+++ htdocs/c.php
@@ -0,0 +1,24 @@
+<?php
+
+    include './cTop.html';
+?>
+
+
+<ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+
+<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+  <a class="nav-link" href="tables.html">
+    <i class="fa fa-fw fa-table"></i>
+    <span class="nav-link-text">Tables</span>
+  </a>
+</li>
+
+</ul>
+
+
+
+
+
+<?php
+    include './cBottom.html';
+?>
