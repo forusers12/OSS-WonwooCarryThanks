78fe690a67d90a83fd7dc5fccdbfc820f466ba9a
WonWooCho <wonwoo7558@gmail.com>
2018-06-18 20:29:26 +0900

    DB오류수정

--- htdocs/managerDB.php
+++ htdocs/managerDB.php
@@ -21,8 +21,7 @@ $dest_m = basename($_FILES['printPic']['name']);
 move_uploaded_file($source_m, "$uploads_dir/$dest_m");
 
 
-$data_stream = "'".$_POST['funcName']."','".$_POST['head']."','".$_POST['beginExplain']."','".$_POST['parameter']."','".$_POST['returnValue']."','".$_POST['returnValue2']"','".$uploads_dir."','".$dest."','".$dest_m."'";
-
+$data_stream = "'".$_POST['funcName']."','".$_POST['head']."','".$_POST['beginExplain']."','".$_POST['parameter']."','".$_POST['returnValue']."','".$_POST['returnValue2']."','".$uploads_dir."','".$dest."','".$dest_m."'";
 $fName = $_POST['funcName'];
 $hName = $_POST['head'];
 $beName = $_POST['beginExplain'];
@@ -32,7 +31,6 @@ $rv2Name = $_POST['returnValue2'];
 
 
 $query = "INSERT INTO `function`(`funcName`, `head`, `beginExplain`, `parameter`, `returnValue`, `returnValue2`, `route`, `exPic`, `printPic`) VALUES (".$data_stream.") ON DUPLICATE KEY UPDATE funcName='$fName', head='$hName', beginExplain='$beName', parameter='$paraName', returnValue='$rv1Name', returnValue2='$rv2Name', route='$uploads_dir', exPic='$dest', printPic='$dest_m'";
-
   $result = mysqli_query($connect, $query);
 
 
